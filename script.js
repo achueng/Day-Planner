712abbb7e8a6046668f09bfed1b19d5dcc56abc3
Agnes Chueng <agneschueng@gmail.com>
2020-10-04 23:11:10 -0500

    created for loop to get data-hr attribute values

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Ensure HTML loads before JavaScript runs
 $(document).ready(function() {
     
-    // Declare/Define global variables
+    // Declare/Define global variables & DOM elements
     var currentHour = parseInt(moment().hour(Number).format("HH"));
-    // console.log(currentHour);
+    // console.log(currentHour); // yield number of current hour
+    var hoursList = document.querySelectorAll(".hour"); // array of hour divs
+    var dataHour;
     
     // Get date to display using .text to get API (moment) data to appear in the <p id="currentDay"> tag
         // Syntax for working with moment.js
@@ -15,8 +17,13 @@ $(document).ready(function() {
         $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
 
     // Textarea input works with .past, .present, .future classes, and will be working with moment.js
+        // Retreive data-hr value from hour divs
+        for (var i=0; i<hoursList.length; i++) {
+            dataHour = hoursList[i].getAttribute("data-hr");
+            // console.log(dataHour);
+        }
         // Using toggle logic & conditionals (if/else)
-            // Add/remove classes .past, .present, .future --> .toggleClass()
+            // Add/remove classes .past, .present, .future --> .classList.add/remove
             // Based on the current hour --> moment().hour(Number);
                 // Format - moment(currentHour).format("hh a");
                 // Use moment().isBefore("09 AM", "hour"); - isAfter, isSame
