727355235a94936a90c4693424cd80030b47f020
Agnes Chueng <agneschueng@gmail.com>
2020-10-04 22:45:49 -0500

    define global variable

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 $(document).ready(function() {
     
     // Declare/Define global variables
-    
+    var currentHour = parseInt(moment().hour(Number).format("HH"));
+    // console.log(currentHour);
     
     // Get date to display using .text to get API (moment) data to appear in the <p id="currentDay"> tag
         // Syntax for working with moment.js
         // moment().year(year).month(month).date(day)
             // to get current date: moment()._d;
             // to get format: m.format(dddd, MMMM Do YYYY);
-        // console.log(moment()._d);
-        // console.log(moment().format('dddd, MMMM Do YYYY'));
+        // console.log(moment()._d); // yields object of current day
+        // console.log(moment().format('dddd, MMMM Do YYYY')); // yields string
         $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
 
     // Textarea input works with .past, .present, .future classes, and will be working with moment.js
